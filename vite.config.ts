9668391ab36d8bb06d255a2c5f0f4bd97b563aa9
Владимир Шевченко <tiigrus@gmail.com>
2026-02-07 10:28:57 +0200

    chore: update package dependencies and configure image optimization plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,13 @@
 import type { UserConfig } from 'vite'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default {
     base: "./",
+    plugins: [
+        ViteImageOptimizer({
+            jpg: {
+                quality: 85
+            }
+        })
+    ]
 } satisfies UserConfig;
